eddaa2e5010d4dd123ad4392648609f056ced95a
apolo <danielrivera076783@gmail.com>
2022-10-22 09:20:17 -0600

    Primer Ejercicio

---
+++ Ejercicio1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Ejercicio1
+ */
+public class Ejercicio1 {
+  
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.print("Introdusca la cantidad de alumnos : ");
+        int totalAlumnos = scanner.nextInt();
+        String carnets[] = new String[totalAlumnos], nombres[] = new String[totalAlumnos];
+        double notas[] = new double[totalAlumnos];
+        
+        for(int i = 0; i < totalAlumnos; i++){
+            System.out.print("Introdusca la nota del alumno " + (i+1)+ " :") ;
+            notas[i] = scanner.nextDouble(); 
+            System.out.print("Intoduca el carnet del alumno " +(i+1)+ " :");
+            carnets[i] = scanner.next();
+            System.out.print("Introduce el nombre del alumno "+(i+1)+ " :");
+            nombres[i] = scanner.next();
+        }
+
+        System.out.println("-------------------  Detalle de Alumnos ---------------------");
+        System.out.println("carnet\t\t\tnombre\t\t\tnota");
+        System.out.println("-------------------------------------------------------------");
+        for(int i = 0; i < totalAlumnos ; i++){
+            System.out.println(carnets[i] 
+            + "\t\t\t" + nombres[i] + "\t\t\t" + notas[i]);
+        }
+      scanner.close();  
+    }
+    
+}
