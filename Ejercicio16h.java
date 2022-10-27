0c7cc2038589bc2382baa8e2a367dcece6e78b27
apolo <danielrivera076783@gmail.com>
2022-10-26 22:46:29 -0600

    Ultimo ejercicio

--- Ejercicio16h.java
+++ Ejercicio16h.java
@@ -1,14 +1,14 @@
-public class Ejercicio16g {
+public class Ejercicio16h {
     public static void main(String[] args) {
         String tabla[][] = new String[10][10];
-
+        String auxTabla[][] = new String[10][10];
         int i, j, k;
         for (i = 0 ; i < 5; i++ ) {
             for (j = 0 ; j < i; j++ ) {
                 tabla[i][j] = " ";
             }
             for(k = i; k < 5 ; k++) {
-                tabla[i][k] = "* ";
+                tabla[i][k] = "*";
             }
         }
         for (i = 4; i >= 0; i-- ) {
@@ -16,14 +16,28 @@ public class Ejercicio16g {
                 tabla[9-i][j] = " ";
             }
             for(k = i; k <= 4; k++) {
-                tabla[9-i][k] = "* ";
+                tabla[9-i][k] = "*";
+            }
+        }
+
+        for(i = 0; i<10 ; i++){
+            for(j = 0 ; j < 10; j++ ){
+                if(tabla[j][i] == null){
+                    tabla[j][i] = " ";
+                }
+                auxTabla[i][j] = tabla[j][i];
+            }
+        }
+        for(i = 4; i>=0 ; i--){
+            for(j = 0 ; j < 10; j++ ){
+               auxTabla[8-i][j] = auxTabla[i][j];
             }
         }
 
         for( i = 0; i <10 ; i++){
             for( j = 0; j<10; j++){
                 if(!(tabla[i][j] == null))
-                    System.out.print(tabla[i][j]);
+                    System.out.print(auxTabla[i][j]);
             }
             System.out.println();
         }
