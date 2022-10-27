04bec0d4a7dfcede19f6266b60703ba83565c72e
apolo <danielrivera076783@gmail.com>
2022-10-26 21:00:26 -0600

    Cambios del 5

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -16,13 +16,14 @@ class Ejercicio4{
         int i;
 
         for (i=0;i<num_clientes;i++){
-        	System.out.print("Ingrese El Nombre Del Cliente "+i+1+" : " );
+			sc.nextLine();
+        	System.out.print("Ingrese El Nombre Del Cliente "+(i+1)+" : " );
         	nombre_clien[i] = sc.nextLine();
-        	System.out.print("Ingrese El Telefono Del Cliente "+i+1+" : ");
+        	System.out.print("Ingrese El Telefono Del Cliente "+(i+1)+" : ");
         	telefono[i] = sc.nextLine();
-        	System.out.print("Ingrese La Direccion Del Cliente "+i+1+" : ");
+        	System.out.print("Ingrese La Direccion Del Cliente "+(i+1)+" : ");
         	direccion[i] = sc.nextLine();
-        	System.out.print("Ingrese La Inversion Del Cliente "+i+1+" : ");
+        	System.out.print("Ingrese La Inversion Del Cliente "+(i+1)+" : ");
         	inversion[i] = sc.nextDouble();
         	if (inversion[i]>=10.00 && inversion[i]<=20.00)
         	{
@@ -39,28 +40,28 @@ class Ejercicio4{
           
         }
      
-        System.out.println("------------------------Detalles De Clientes General--------------------------------");
+        System.out.println("----------Detalles De Clientes General------------");
         System.out.println("Nombre Del Cliente\t\t\tTelefono\t\t\tDireccion Del Cliente\t\t\tInversion\t\t\tPorcentaje De Descuento\t\t\tMonto A Cancelar");
         for (i=0;i<num_clientes;i++)
         {
         	System.out.println(nombre_clien[i]+"\t\t\t"+telefono[i]+"\t\t\t"+direccion[i]+"\t\t\t"+inversion[i]+"\t\t\t"+porcentaje[i]+"\t\t\t"+monto_cancelar);
         }
         
-        System.out.println("------------------------Detalles De Clientes Con 10% Descuento--------------------------------");
+        System.out.println("-----Detalles De Clientes Con 10% Descuento-------");
         for (i=0;i<num_clientes;i++)
         {
         	if (porcentaje[i] == "10%")
         	System.out.println(nombre_clien[i]+"\t\t\t"+telefono[i]+"\t\t\t"+direccion[i]+"\t\t\t"+inversion[i]+"\t\t\t"+porcentaje[i]+"\t\t\t"+monto_cancelar);
         }
 
-         System.out.println("------------------------Detalles De Clientes Con 15% Descuento--------------------------------");
+         System.out.println("----Detalles De Clientes Con 15% Descuento--------");
         for (i=0;i<num_clientes;i++)
         {
         	if (porcentaje[i] == "15%")
         	System.out.println(nombre_clien[i]+"\t\t\t"+telefono[i]+"\t\t\t"+direccion[i]+"\t\t\t"+inversion[i]+"\t\t\t"+porcentaje[i]+"\t\t\t"+monto_cancelar);
         }
         
-         System.out.println("------------------------Detalles De Clientes Con 20% Descuento--------------------------------");
+         System.out.println("--------Detalles De Clientes Con 20% Descuento----------");
         for (i=0;i<num_clientes;i++)
         {
         	if (porcentaje[i] == "20%")
--- Ejercicio5.java
+++ Ejercicio5.java
@@ -6,12 +6,34 @@ public class Ejercicio5 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introdusca la cantidad de Empleados : ");
         int cantidadEmpleados = scanner.nextInt();
-        double descuento = 0.10;
+
         double[] sueldoBase = new double[cantidadEmpleados];
         int[] horasExtra = new int[cantidadEmpleados];
         double[] descuentos = new double[cantidadEmpleados];
+        double[] sueldoNeto = new double[cantidadEmpleados];
+        String nombres[] = new String[cantidadEmpleados];
+        String codigo[] = new String[cantidadEmpleados];
+        System.out.print("Introdusca el porcentaje de Descuentos : ");
+        double descuento = scanner.nextDouble()/100.00d;
+        System.out.print("Introdusca el valor de la hora extra :");
+        double pagoHora = scanner.nextDouble();
 
-assa
+        for( int i = 0 ; i < cantidadEmpleados ; i++){
+            System.out.print("Introdusca el Nombre del Empleado "  + (i+1)+" :");
+            nombres[i] = scanner.next();
+            System.out.print("Introdusca el Codigo de Empleado "+ (i+1)+" :");
+            codigo[i] = scanner.next();
+            System.out.println("Introdusca el sueldo Base del Empleado: "+ (i+1)+" :");
+            sueldoBase[i] = scanner.nextDouble();
+            System.out.println("Introdusca la cantidad de Horas Extra");
+            horasExtra[i] = scanner.nextInt();
+        }
+        System.out.println("Codigo\tNombre\tSueldo Neto");
+        for( int i = 0 ; i < cantidadEmpleados ; i++){
+            descuentos[i] = sueldoBase[i]*descuento;
+            sueldoNeto[i] = sueldoBase[i] - descuentos[i] + pagoHora*horasExtra[i];
+            System.out.println(codigo[i] + "\t" + nombres[i] + "\t" + sueldoNeto[i] );
+        }
         scanner.close();
     }
 }
